f3c4828bbb6baccc65e8a67526fa2b41b0569bb8
Harold A. Speights <harold.a.speights@gmail.com>
2015-08-10 03:54:10 -0400

    Assignment 22 completed

---
+++ models/address_book.rb
@@ -0,0 +1,28 @@
+require_relative "entry.rb"
+ 
+ class AddressBook
+   attr_accessor :entries
+
+   def initialize
+     @entries = []
+   end
+   
+   def add_entry(name, phone, email)
+ # #9
+     index = 0
+     @entries.each do |entry|
+ # #10
+       if name < entry.name
+         break
+       end
+       index += 1
+     end
+ # #11
+     @entries.insert(index, Entry.new(name, phone, email))
+   end
+
+   def remove_entry(email)
+    @entries.delete_if {|entry| entry.email == email}
+  end
+
+end
